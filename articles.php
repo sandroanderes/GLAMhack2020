833a120f9a506f86a6789fd0d67898000ace69e5
Frankie96 <frankie_96@windowslive.com>
2020-06-06 00:40:56 +0200

    Update articles.php

--- articles.php
+++ articles.php
@@ -18,7 +18,7 @@ require_once('config/data.php');
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
     <?php 
       $articles = get_all_articles();
-      foreach ($article as $articles){
+      foreach ($articles as $article){
     ?>
         <div class="col mb-4">
           <div class="card">
