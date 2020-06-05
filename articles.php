1ccbf707ee46fcfa84fc2c39c48728929f5587b3
Frankie96 <frankie_96@windowslive.com>
2020-06-06 00:51:15 +0200

    get

--- articles.php
+++ articles.php
@@ -1,5 +1,5 @@
 <!-- ===== Controller ===== -->
-<?php 
+<?php
 // Alle Site-relevanten Werte
 require_once('config/config.php');
 // Alle DB-Abfragen
@@ -13,102 +13,35 @@ require_once('config/data.php');
 
 <!-- ==== CONTENT ==== -->
 <div class="container mt-3">
-    <h1>Articles</h1>
-    <!-- Place content here -->
-    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
-    <?php 
-      $articles = get_all_articles();
-      foreach ($articles as $article){
-    ?>
-        <div class="col mb-4">
-          <div class="card">
-            <img src="<?php echo $article['img_url']; ?>" class="card-img-top">
-            <div class="card-body">
-              <h5 class="card-title"><?php echo $article['title']; ?></h5>
-              <p class="teaser" class="card-text"><?php echo $article['teaser']; ?></p>
-            </div>
-            <button class="btn">Read more</button>
-          </div>
-        </div>
-      <?php } ?>
-        
-        <!--<div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>       
-        <div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>       
-        <div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>       
-        <div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>       
-        <div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>       
-        <div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>       
-        <div class="col mb-4">
-          <div class="card">
-            <img src="assets/img/demo.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
-            </div>
-            <button>Read</button>
-          </div>
-        </div>-->
-      </div>
+  <h1>Articles</h1>
 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+  <!-- Place content here -->
+  <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+    <?php
+    $articles = get_all_articles();
+    foreach ($articles as $article) {
+    ?>
+      <div class="col mb-4">
+        <div class="card">
+          <img src="<?php echo $article['img_url']; ?>" class="card-img-top">
+          <div class="card-body">
+            <h5 class="card-title"><?php echo $article['title']; ?></h5>
+            <p class="teaser" class="card-text"><?php echo $article['teaser']; ?></p>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"><?php echo $article['date']; ?></small>
+          </div>
+          <button class="btn">Read more</button>
+        </div>
+      </div>
+    <?php } ?>
+  </div>
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
 </div>
 
--- config/data.php
+++ config/data.php
@@ -14,6 +14,7 @@ function get_db_connection()
     return $pdo;
 }
 
+// SELECT alle Artikel
 function get_all_articles()
 {
     $pdo = get_db_connection();
@@ -27,42 +28,5 @@ function get_all_articles()
         while ($row = $statement->fetch()) {
             echo $row['title'] . "<br />";
         }
-
-    }    /* 
-    else {
-        echo "SQL Error <br />";
-        echo $statement->queryString . "<br />";
-        echo $statement->errorInfo()[2];
     }
-*/
 }
-
-
-
-
-/* // SELECT everything
-function get_all_articles()
-{
-    $db = get_db_connection();
-    $sql = "SELECT * FROM articles";
-    $result = $db->query($sql);
-    return $result->fetchAll();
-}
-
-// SELECT everything of spez. article
-function get_article_by_id($id)
-{
-    $db = get_db_connection();
-    $sql = "SELECT * FROM articles WHERE id = $id;";
-    $result = $db->query($sql);
-    return $result->fetch();
-}
-
-// SELECT teaser of all
-function get_teaser_articles()
-{
-    $db = get_db_connection();
-    $sql = "SELECT id, title, title_f, teaser, teaser_f, date, img_url, newspaper_name FROM articles";
-    $result = $db->query($sql);
-    return $result->fetch();
-} */
