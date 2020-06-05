03a16f6a34293c78b665518bf961c70a5c8c5e17
Frankie96 <frankie_96@windowslive.com>
2020-06-06 01:02:30 +0200

    Update articles.php

--- articles.php
+++ articles.php
@@ -26,12 +26,10 @@ require_once('config/data.php');
           <img src="<?php echo $article['img_url']; ?>" class="card-img-top">
           <div class="card-body">
             <h5 class="card-title"><?php echo $article['title']; ?></h5>
+            <h6 class="card-subtitle mb-2 text-muted"><?php echo $article['date']?> - <?php echo $article['newspaper_name']?> </h6>
             <p class="teaser" class="card-text"><?php echo $article['teaser']; ?></p>
           </div>
           <button class="btn btn-primary">Read more</button>
-          <div class="card-footer">
-            <small class="text-muted"><?php echo $article['date']; ?></small>
-          </div>
         </div>
       </div>
     <?php } ?>
