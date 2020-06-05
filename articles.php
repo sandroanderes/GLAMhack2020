0a9a68846f9d907436eda32cffa01266c2fcdeb1
Frankie96 <frankie_96@windowslive.com>
2020-06-05 22:34:40 +0200

    Update articles.php

--- articles.php
+++ articles.php
@@ -9,18 +9,19 @@ require_once('config/data.php');
 $servername = "localhost";
 $username = "hacker20";
 $password = "Welcome1";
+$dbname = "GLAMhack2020";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-echo "Connected successfully";
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} 
 
-$sql = "SELECT * FROM 'articles-1914'";
+$sql = "SELECT * FROM articles-1914";
 $result = $conn->query($sql);
+echo $result;
 
 if ($result->num_rows > 0) {
   // output data of each row
