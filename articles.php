226dc494ac95af2e8fcfa757c309e7f7ee1ee7d6
indumid <66444657+indumid@users.noreply.github.com>
2020-06-05 16:38:29 +0200

    Boum

--- articles.php
+++ articles.php
@@ -18,6 +18,95 @@ require_once('config/data.php');
     <h1>Articles</h1>
     <!-- Place content here -->
 
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>       
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>       
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>       
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>       
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>       
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>       
+        <div class="col mb-4">
+          <div class="card">
+            <img src="assets/img//circle-game-ring-100-resimage_v-variantSmall16x9_w-1024.png" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="teaser" class="card-text">Das ist ein Teasertext - er soll nicht zu lang sein. Ein bis zwei Sätze genügen.</p>
+            </div>
+            <button>Read</button>
+          </div>
+        </div>
+      </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </div>
 
 <!-- == FOOTER == -->
