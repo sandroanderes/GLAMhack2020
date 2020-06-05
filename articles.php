d4922bcef3492fac7836450767ddffc97fd6cd43
Frankie96 <frankie_96@windowslive.com>
2020-06-06 00:33:04 +0200

    Update articles.php

--- articles.php
+++ articles.php
@@ -18,6 +18,7 @@ require_once('config/data.php');
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3">
     <?php 
       $articles = get_all_articles();
+      echo $article;
       foreach ($article as $articles){
     ?>
         <div class="col mb-4">
