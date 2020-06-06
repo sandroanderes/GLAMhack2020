dd9eb3df3ec2631bc9db937ddc80815b7940a8bf
baerni-im <61183169+baerni-im@users.noreply.github.com>
2020-06-06 13:57:33 +0200

    Update about.php
    Inhalte eingefügte nur html

--- about.php
+++ about.php
@@ -21,7 +21,28 @@ require_once('templates/header.php');
     <h1>About</h1>
     <!-- Place content here -->
     <section style="padding-top: 40px !important; padding-bottom: unset !important">
-        <h2>Titel</h2>
+        <h2>Über uns</h2>
+        <article>
+            <p>Diese Webseite entstand im Rahmen des 6. Swiss Open Cultural Data Hackathon vom 5. + 6. Juni 2020. Die Motivation hinter diesem Projekt ist es aufzuzeigen, welche Bedeutung der kostenfreie und bedingungslose Zugang zu Kulturdaten für die Gesellschaft haben kann. Unser Projekt ist als Aufforderung an Museen und Bibliotheken zu verstehen, ihre Kulturdaten der Öffentlichkeit zugänglich zu machen. <br><br>
+            Unsere Projektgruppe setzt sich aus folgenden Personen zusammen:<br></p>
+            
+            <ul>
+               <li><a href="https://github.com/sandroanderes" target="_blank">Sandro Anderes</a></li>
+               <li><a href="https://www.indumi.ch/" target="_blank">David Indumi</a></li>
+               <li>Frank Zinsli</li>
+               <li><a href="https://bernhardaebersold.ch/" target="_blank">Bernhard Aebersold</a></li>
+            </ul>
+
+        </article>
+    </section>
+    <section style="padding-top: 40px !important; padding-bottom: unset !important">
+        <h2>Die Daten</h2>
+        <article>
+            <p>Die Daten stammen ursprünglich von der EPFL Lausanne und enthalten die Zeitungsartikel der beiden Schweizer Zeitungen “Gazette de Lausanne” und “Tribune de Genève” aus dem Jahr 1914. Die Daten können auf <a href="https://opendata.swiss/en/dataset/journal-de-geneve-gazette-de-lausanne-1914" target="_blank">Open Data</a> abgerufen werden.</p>
+        </article>
+    </section>
+    <section style="padding-top: 40px !important; padding-bottom: unset !important">
+        <h2>Das Projekt</h2>
         <article>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, temporibus fuga. Magni obcaecati, voluptatibus cumque veniam odio nulla esse vero autem minima. Blanditiis molestias eos, impedit porro aut accusantium adipisci?</p>
         </article>
@@ -29,19 +50,7 @@ require_once('templates/header.php');
     <section style="padding-top: 40px !important; padding-bottom: unset !important">
         <h2>Titel</h2>
         <article>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, temporibus fuga. Magni obcaecati, voluptatibus cumque veniam odio nulla esse vero autem minima. Blanditiis molestias eos, impedit porro aut accusantium adipisci?</p>
-        </article>
-    </section>
-    <section style="padding-top: 40px !important; padding-bottom: unset !important">
-        <h2>Titel</h2>
-        <article>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, temporibus fuga. Magni obcaecati, voluptatibus cumque veniam odio nulla esse vero autem minima. Blanditiis molestias eos, impedit porro aut accusantium adipisci?</p>
-        </article>
-    </section>
-    <section style="padding-top: 40px !important; padding-bottom: unset !important">
-        <h2>Titel</h2>
-        <article>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, temporibus fuga. Magni obcaecati, voluptatibus cumque veniam odio nulla esse vero autem minima. Blanditiis molestias eos, impedit porro aut accusantium adipisci?</p>
+            <p>Die diesjährige Ausgabe des Swiss Open Cultural Data Hackathon fand vom 5. bis 6. Juni 2020 als reiner Online-Hackathon statt. Der Event wurde virtuell vom Schweizerischen Institut für Informationswissenschaft an der Fachhochschule Graubünden in Zusammenarbeit mit dem Institut für Multimedia-Produktion durchgeführt. Der Schwerpunkt des GLAMhack liegt auf Linked Open Data, Maschinelles Lernen, Mensch-Computer-Interaktion und Crowdsourcing.</p>
         </article>
     </section>
 </div>
