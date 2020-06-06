8c87493ce2627cea085ef8a90f5199337fa5b5a6
Frankie96 <frankie_96@windowslive.com>
2020-06-06 03:56:50 +0200

    Update articles.php

--- articles.php
+++ articles.php
@@ -29,15 +29,15 @@ require_once('config/data.php');
             <h6 class="card-subtitle mb-2 text-muted"><?php echo $article['newspaper_name']?> - <?php echo $article['date']?> </h6>
             <p class="teaser" class="card-text"><?php echo $article['teaser']; ?></p>
           </div>
-          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#<?php echo $article['id']; ?>ModalCenter">
-          Mehr lesen
-        </button>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+  Launch demo modal
+</button>
 
-        <div class="modal fade" id="<?php echo $article['id']; ?>ModalCenter" tabindex="-1" role="dialog" aria-labelledby="<?php echo $article['id']; ?>ModalCenterTitle" aria-hidden="true">
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="<?php echo $article['id']; ?>ModalLongTitle">Sprache auswählen</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
