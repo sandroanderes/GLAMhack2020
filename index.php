82dd8f09211d3a481b048b2b7df7f7c001452829
Frankie96 <frankie_96@windowslive.com>
2020-06-06 02:56:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,7 @@ require_once "templates/header.php";
       <div class="timeline-img"></div>
 
       <div class="timeline-content timeline-card js--fadeInLeft">
-        <div class="timeline-img-header">
-          <img style="width: 1000; height: 800; max-width: 100px" src="<?php echo $article['img_url']; ?>">
+        <div class="timeline-img-header" style="background-image:url(<?php echo $article['img_url'];?>)">
           <h2><?php echo $article['title']; ?></h2>
         </div>
         <div class="date"><?php echo $article['date']; ?></div>
